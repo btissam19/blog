6157e0e8e3dca26fe27a037289513ac2b7d28f63
ibtissam ech-chaibi <123760378+btissam19@users.noreply.github.com>
2023-04-29 23:18:11 +0000

    Add files via upload

---
+++ 2023_04_29_103548_posts.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void{
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug');
+            $table->text('title');
+            $table->longText('description');
+            $table->string('image_path');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+      }
+
+
+
+
+    public function down(): void{Schema::dropIfExists('failed_jobs');}
+};
